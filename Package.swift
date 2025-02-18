47a35c271e7a0eef803c89bcf249e96bdc6f8408
v-odemir <111747063+v-odemir@users.noreply.github.com>
2025-02-18 10:51:32 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "EdgeSdk",
-            url:"https://github.com/v-odemir/MobileFrameworkTest/releases/download/v1.0.4/EdgeSdk.xcframework.zip",
+            url:"https://github.com/v-odemir/MobileFrameworkTest/releases/download/v1.0.5/EdgeSdk.xcframework.zip",
             checksum: "2eff506030ebfe2e032d96cb49dd47f3193915c5edcf6fb3602845af3fca8823"
         ),
     ]

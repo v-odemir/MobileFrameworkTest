7889044dfb352834e04ded0a2b1fb14318f8797d
Omer Demir <odemir@valven.com>
2025-02-19 20:32:33 +0300

    update

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "EdgeSdk",
-            url:"https://github.com/v-odemir/MobileFrameworkTest/releases/download/v1.2.4/EdgeSdk.xcframework.zip",
-            checksum: "067fb8d55e768d9d4d4afe31256405217d546be0a5d21a5e5db1c44355fc42b4"
+            url:"https://github.com/v-odemir/MobileFrameworkTest/releases/download/v1.2.5/EdgeSdk.xcframework.zip",
+            checksum: "ffe2448803a7e324f58f3c2494c70c885ebed793b2aa5f0eb1ed6c3007cc690d"
         ),
     ]
 )
